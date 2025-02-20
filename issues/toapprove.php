c1e2cf94f180c0c1495f354f98c1f41de579cf5b
Kacper Wyczawski <kacper@wyczawski.dev>
2025-02-20 15:21:48 +0100

    feat: message when there are no issues to approve

--- issues/toapprove.php
+++ issues/toapprove.php
@@ -21,52 +21,59 @@ require "../sidebar.php";
 ?>
 <main>
     <h1>Zgłoszenia do zatwierdzenia</h1>
-    <table>
-        <tr>
-            <th>Sala</th>
-            <th>Opis</th>
-            <th>Priorytet</th>
-            <th>Wykonawca</th>
-            <th>Akcja</th>
-        </tr>
-        <?php
-        require "../db.php";
-        $stmt = $db->prepare("select issue_id, room, description from issues where priority is null");
-        $stmt->execute();
-        foreach ($stmt as $row):
-        ?>
+    <?php
+    require "../db.php";
+    $stmt = $db->prepare("select issue_id, room, description from issues where priority is null");
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    if (!$result):
+    ?>
+        <p>
+            No i fajrant, brak zgłoszeń do zatwierdzenia.
+        </p>
+    <?php else: ?>
+        <table>
             <tr>
-                <td><?= $row["room"] ?></td>
-                <td><?= $row["description"] ?></td>
-                <td colspan="3">
-                    <form method="post">
-                        <select name="priority" required>
-                            <option value="high">Wysoki</option>
-                            <option value="normal" selected>Normalny</option>
-                            <option value="low">Niski</option>
-                        </select>
-                        <select name="user_id" required>
-                            <?php
-                            $stmt2 = $db->prepare("select user_id, first_name, last_name from users where role = 'executor'");
-                            $stmt2->execute();
-                            foreach ($stmt2 as $row2):
-                            ?>
-                                <option value="<?= $row2["user_id"] ?>"><?= $row2["first_name"] ?> <?= $row2["last_name"] ?></option>
-                            <?php
-                            endforeach;
-                            ?>
-                        </select>
-                        <input type="hidden" name="id" value="<?= $row["issue_id"] ?>">
-                        <button type="submit" name="approve">Zatwierdź</button>
-                    </form>
-                    <form method="post">
-                        <input type="hidden" name="id" value="<?= $row["issue_id"] ?>">
-                        <button type="submit" name="reject">Odrzuć</button>
-                    </form>
-                </td>
+                <th>Sala</th>
+                <th>Opis</th>
+                <th>Priorytet</th>
+                <th>Wykonawca</th>
+                <th>Akcja</th>
             </tr>
-        <?php
-        endforeach;
-        ?>
-    </table>
+            <?php
+            foreach ($result as $row):
+            ?>
+                <tr>
+                    <td><?= $row["room"] ?></td>
+                    <td><?= $row["description"] ?></td>
+                    <td colspan="3">
+                        <form method="post">
+                            <select name="priority" required>
+                                <option value="high">Wysoki</option>
+                                <option value="normal" selected>Normalny</option>
+                                <option value="low">Niski</option>
+                            </select>
+                            <select name="user_id" required>
+                                <?php
+                                $stmt = $db->prepare("select user_id, first_name, last_name from users where role = 'executor'");
+                                $stmt->execute();
+                                foreach ($stmt as $row2):
+                                ?>
+                                    <option value="<?= $row2["user_id"] ?>"><?= $row2["first_name"] ?> <?= $row2["last_name"] ?></option>
+                                <?php
+                                endforeach;
+                                ?>
+                            </select>
+                            <input type="hidden" name="id" value="<?= $row["issue_id"] ?>">
+                            <button type="submit" name="approve">Zatwierdź</button>
+                        </form>
+                        <form method="post">
+                            <input type="hidden" name="id" value="<?= $row["issue_id"] ?>">
+                            <button type="submit" name="reject">Odrzuć</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php endif; ?>
 </main>
