8eab5930d67d47059a82f717ab23da77189cdfcb
Kacper Wyczawski <kacper@wyczawski.dev>
2025-02-20 16:32:02 +0100

    feat: center index text

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require_once('sidebar.php');
 ?>
 <main>
     <?php if (isset($_SESSION["username"])): ?>
-        <h1>Dzień dobry, <?= $_SESSION["first_name"] ?>!</h1>
+        <h1 class="index">Dzień dobry, <?= $_SESSION["first_name"] ?>!</h1>
     <?php else: ?>
-        <h1>
+        <h1 class="index">
             Witaj! <a href="/login.php">Zaloguj się</a>.
         </h1>
     <?php endif; ?>
