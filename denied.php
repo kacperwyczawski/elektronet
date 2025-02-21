3d833377017dccfb024ef73aca6288cc10f2649b
Kacper Wyczawski <kacper@wyczawski.dev>
2025-02-21 14:46:28 +0100

    feat: enhance denied page with navigation link

--- denied.php
+++ denied.php
@@ -5,5 +5,8 @@ require_once('sidebar.php');
 <main>
     <h1 class="index">
         Niestety nie masz dostępu do tej strony. Jeśli uważasz, że to błąd, poproś dyrekcję o stworzenie nowego konta z wyższymi uprawnieniami.
+        <br>
+        <br>
+        <a href="/">Powrót do strony głównej</a>
     </h1>
 </main>
