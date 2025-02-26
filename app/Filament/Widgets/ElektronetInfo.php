537cd6591feff1451c91691732bf27d7ecbfef41
Kacper Wyczawski <kacper@wyczawski.dev>
2025-02-26 14:43:09 +0100

    feat: replace filament widget with elektronet

---
+++ app/Filament/Widgets/ElektronetInfo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class ElektronetInfo extends Widget
+{
+    protected static string $view = 'filament.widgets.elektronet-info';
+}
