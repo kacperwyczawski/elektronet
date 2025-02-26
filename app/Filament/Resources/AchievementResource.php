d0437cf26b0ec7970d83457067681e38d5a5e608
Kacper Wyczawski <kacper@wyczawski.dev>
2025-02-26 14:15:58 +0100

    refactor: regenerate the achievement resource

--- app/Filament/Resources/AchievementResource.php
+++ app/Filament/Resources/AchievementResource.php
@@ -5,15 +5,10 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\AchievementResource\Pages;
 use App\Filament\Resources\AchievementResource\RelationManagers;
 use App\Models\Achievement;
-use Faker\Provider\ar_EG\Text;
 use Filament\Forms;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
@@ -35,21 +30,18 @@ class AchievementResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')
+                Forms\Components\TextInput::make('name')
                     ->required()
-                    ->maxLength(255)
                     ->label('Nazwa'),
-                TextInput::make('result')
+                Forms\Components\TextInput::make('result')
                     ->required()
-                    ->maxLength(255)
                     ->label('Wynik'),
-                DatePicker::make('achieved_at')
+                Forms\Components\DatePicker::make('achieved_at')
                     ->required()
                     ->maxDate(now())
                     ->label('Data osiągnięcia'),
-                Select::make('student_id')
+                Forms\Components\Select::make('student_id')
                     ->required()
-                    ->label('Uczeń')
                     ->relationship('student')
                     ->getOptionLabelFromRecordUsing(fn(Model $record) =>
                         $record->first_name . ' ' .
@@ -58,15 +50,15 @@ class AchievementResource extends Resource
                     ->searchable()
                     ->preload()
                     ->createOptionForm([
-                        TextInput::make('first_name')
+                        Forms\Components\TextInput::make('first_name')
                             ->required()
-                            ->maxLength(255),
-                        TextInput::make('last_name')
+                            ->label('Imię'),
+                        Forms\Components\TextInput::make('last_name')
                             ->required()
-                            ->maxLength(255),
-                        TextInput::make('school_id_number')
+                            ->label('Nazwisko'),
+                        Forms\Components\TextInput::make('school_id_number')
                             ->required()
-                            ->maxLength(255), // TODO: regex validation
+                            ->label('Numer legitymacji'),
                     ])
             ]);
     }
@@ -75,24 +67,31 @@ class AchievementResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('name')
+                Tables\Columns\TextColumn::make('name')
                     ->searchable()
                     ->label('Nazwa'),
-                TextColumn::make('result')
+                Tables\Columns\TextColumn::make('result')
                     ->searchable()
                     ->label('Wynik'),
-                TextColumn::make('achieved_at')
+                Tables\Columns\TextColumn::make('achieved_at')
                     ->searchable()
-                    ->sortable()
                     ->label('Data osiągnięcia'),
-                TextColumn::make('student.first_name')
+                Tables\Columns\TextColumn::make('student.first_name')
                     ->searchable()
                     ->label('Imię'),
-                TextColumn::make('student.last_name')
+                Tables\Columns\TextColumn::make('student.last_name')
                     ->searchable()
                     ->label('Nazwisko'),
-                TextColumn::make('student.school_id_number')
+                Tables\Columns\TextColumn::make('student.school_id_number')
                     ->label('Numer legitymacji'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
