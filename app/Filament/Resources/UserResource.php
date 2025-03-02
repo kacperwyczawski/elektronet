b33545c9d1a820a3cf8f55e941eacbc3688407c0
Kacper Wyczawski <kacper@wyczawski.dev>
2025-03-02 15:36:06 +0100

    feat: add user view

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -128,6 +128,7 @@ class UserResource extends Resource
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
