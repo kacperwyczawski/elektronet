b5a71249897d108445b23083caac514750fa2e40
Kacper Wyczawski <kacper@wyczawski.dev>
2025-03-06 17:36:13 +0100

    feat: disable deleting and editing issue assignments from UI

--- app/Filament/Resources/IssueAssignmentResource.php
+++ app/Filament/Resources/IssueAssignmentResource.php
@@ -93,12 +93,10 @@ class IssueAssignmentResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                //
             ]);
     }
 
@@ -114,7 +112,6 @@ class IssueAssignmentResource extends Resource
         return [
             'index' => Pages\ListIssueAssignments::route('/'),
             'view' => Pages\ViewIssueAssignment::route('/{record}'),
-            'edit' => Pages\EditIssueAssignment::route('/{record}/edit'),
         ];
     }
 }
